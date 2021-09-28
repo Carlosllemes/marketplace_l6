9131b7eb524c8f1de74e05245d411f3dff06f659
Carlos Lemes <carlos.lemes@doutoresdaweb.com.br>
2021-09-28 18:03:27 -0300

    toggle

--- public/js/scripts/deleteimg.js
+++ public/js/scripts/deleteimg.js
@@ -2,11 +2,17 @@ $('.btn-danger').on('click', function (e){
     e.preventDefault();
     let el = $(this);
 
+    el.children().toggleClass('fa-trash fa-spinner').addClass('fa-spin');
+
+
     $.ajax({
         url: window.location.origin + '/admin/products/delete/' +  el.data('name'),
         type:'GET',
         success: function(result){
-            el.parent().remove();
+
+            el.parent().fadeOut('slow', function (){
+                $(this).remove()
+            });
         },
         error: function(){console.log('error')},
     })
